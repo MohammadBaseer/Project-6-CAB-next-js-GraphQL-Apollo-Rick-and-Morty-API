36b86ac7ebbebaa6e5744f03cdb86cb0eb8061e8
Baseer Rahimi <mohammadbaseer25@gmail.com>
2024-07-18 12:50:37 +0200

    fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,13 @@ export default function Home() {
     <main className={styles.main}>
       <h2>Homepage</h2>
       <br />
-      <h3>I&aposm learning about rendering styles in Next.js - this page was Statically Rendered by default.</h3>
+      <h3>I&apos;m learning about rendering styles in Next.js - this page was Statically Rendered by default.</h3>
       <br />
-      <h3>The Client Side Characters page is rendered Client-Side and shows a list of Rick and Morty characters. From there, you can click each character to follow a Statically-Generated path, and visit Character page rendering with Static-Site-Generation. I&aposve fetched the data for these pages from a REST API.</h3>
+      <h3>The Client Side Characters page is rendered Client-Side and shows a list of Rick and Morty characters. From there, you can click each character to follow a Statically-Generated path, and visit Character page rendering with Static-Site-Generation. I&apos;ve fetched the data for these pages from a REST API.</h3>
       <br />
-      <h3>The Server Side Characters page is rendered Server-Side and shows a list of Rick and Morty characters. From there, you can click each character to follow a Statically-Generated path, and visit Character page rendering with Static-Site-Generation. I&aposve fetched the data for these pages from a REST API.</h3>
+      <h3>The Server Side Characters page is rendered Server-Side and shows a list of Rick and Morty characters. From there, you can click each character to follow a Statically-Generated path, and visit Character page rendering with Static-Site-Generation. I&apos;ve fetched the data for these pages from a REST API.</h3>
       <br />
-      <h3>The Episodes page is rendered Server-Side. I&aposve used a filtered GraphQL query to fetch the data. Here is a good article to help you use Graph QL together with the Fetch API.</h3>
+      <h3>The Episodes page is rendered Server-Side. I&apos;ve used a filtered GraphQL query to fetch the data. Here is a good article to help you use Graph QL together with the Fetch API.</h3>
       <br />
     </main>
   );
